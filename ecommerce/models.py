0f92024069a8cb280d0abe64221b5cccc0eeec90
CasterCodes <nyagucha.kevin.otwori@gmail.com>
2024-07-29 13:26:22 +0300

    feat: implement get and display product

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,9 +1,32 @@
 from flask_login import UserMixin
 from . import db
+from datetime import datetime
+from sqlalchemy import func
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable=False)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    products = db.relationship('Product', backref='category', lazy=True)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    discount_price = db.Column(db.Float, nullable=True)
+    stock = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(200), nullable=True)
+    is_active = db.Column(db.Boolean, default=True, nullable=False)
+    is_featured = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now())
+    updated_at = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
     
--- ecommerce/products.py
+++ ecommerce/products.py
@@ -1,13 +1,19 @@
 from flask import Blueprint, render_template, request, redirect
 from os import getenv
 from flask_login import login_required, current_user
+from .models import Product
 
 products = Blueprint('products', __name__)
 
 
 @products.route('/')
 def index():
-    return render_template("index.html",current_user=current_user)
+    products = Product.query.all()
+    return render_template("index.html",current_user=current_user, products=products)
+
+@products.route('/<product_id>', methods=['GET'])
+def product(product_id):
+    return product_id
 
 @products.route("/cart")
 @login_required
